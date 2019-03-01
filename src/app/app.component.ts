66e189967eca679ea5ee389025572c856f779da7
samchacko <sam.chacko.2015@gmail.com>
2019-03-01 13:05:39 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,12 @@
+//   Requirements
+//   Step 1
+//    Add the following properties and values to the AppComponent class within the app.component.ts file:
+//
+//       City - include the city you live in.
+//       Tag line - make up a unique tag line.
+//       About Me - a quick paragraph about you.
+
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +15,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularL03';
+  title = 'Mr. Sam Chacko';
+  City = "Houston, TX";
+  tagLine = "Thank you Bethel School of Technology & Woz-U!";
+  AboutMe = "I code in Ruby on Rails. And after this course in Angular also. Trying to  find a job";
 }
